4f77c3ab8324d7a8acfdc6fe1ffc12f2852f69de
geo-j <g.juglan@tutanota.com>
2021-05-10 13:52:19 +0200

    handle arrivals

--- sim.py
+++ sim.py
@@ -5,11 +5,13 @@ unique = count()
 
 N_DAYS = 7
 CAR_DEVIATION = 50
+FRAME = 10000
+PARKING_CHECKS = 3
 
 N_CARS = 50
 N_PARKING_SPOTS = 7
 PARKING_SPOTS = [60, 80, 60, 70, 60, 60, 50]
-PARKING_PREFERENCE = [15, 15, 15, 20, 15, 10, 10]
+PARKING_PREFERENCE = [0.15, 0.15, 0.15, 0.20, 0.15, 0.10, 0.10]
 PRICE_PER_KWH = [16, 16, 18, 18, 22, 20]
 N_CABLES = 9
 
@@ -33,10 +35,27 @@ class CarEvent(Event):
 
 class Arrival(CarEvent):
     def event_handler(self):
-        print("A car wants to park from " + str(self.car.arrival_hour) + " until " + str(self.car.planned_departure))
+        parking_location = np.random.choice(a = len(PARKING_PREFERENCE), size = PARKING_CHECKS, replace = False, p = PARKING_PREFERENCE)
+
+        found_spot = False
+        checks = -1
+        while not found_spot and checks != 2:
+            checks += 1
+            found_spot = (PARKING_SPOTS[parking_location[checks]] > state.parking_spots_used[parking_location[checks]])       # check whether there is a free parking spot
+
+        if found_spot:
+            state.parking_spots_used[parking_location[checks]] += 1
+            print("A car wants to park from " + str(self.car.arrival_hour) + " until " + str(self.car.planned_departure) + " at " + str(parking_location[checks]))
+            print("There are now " + str(state.parking_spots_used[parking_location[checks]]) + " free spots remaining.")
+            print(f"A car wants to park from {str(self.car.arrival_hour)} until {str(self.car.planned_departure)} at {str(parking_location[checks])}")
+        else:
+            print("A car left")
+        # print(parking_location.len)
+        # 
+        # either leave, or park
 
 class StartCharging(CarEvent):
-    pass
+        pass
 
 class StopCharging(CarEvent):
     pass
@@ -92,15 +111,12 @@ def init():
 
     for i in range(N_DAYS):
         n_cars = int(np.random.normal(loc = N_CARS, scale = CAR_DEVIATION))
-        cars = [Car(arrival_hour = i * 24 + np.random.randint(len(arrival_hours)), 
-        connection_time = np.random.randint(len(connection_times)), 
-        charging_volume = np.random.randint(len(charging_volumes))) for i in range(n_cars)]
+        cars = [Car(arrival_hour = i * 24 + np.random.randint(len(arrival_hours)) + np.random.randint(FRAME) / float(FRAME),    # generate increasing arrival times for each of the N_DAYS
+        connection_time = np.random.randint(len(connection_times)) + np.random.randint(FRAME) / float(FRAME),                   # generate a connection time from the aggregate interval, and add a generated subunit of hour 
+        charging_volume = np.random.randint(len(charging_volumes)) + np.random.randint(FRAME) / float(FRAME)) for i in range(n_cars)] # generate a connection time from the aggregate interval, and add a generate subunit of hour for each car
         for car in cars:
             events.put((car.arrival_hour, next(unique), Arrival(car)))
-        # print(cars[len(cars) - 1].arrival_hour)
     
-    # print(cars)
-
 
 if __name__ == '__main__':
     init()
