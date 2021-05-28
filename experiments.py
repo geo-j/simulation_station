114af2b99afc88a321c780399d1f9269e55f4c49
geo-j <g.juglan@tutanota.com>
2021-05-28 12:35:40 +0200

    modified output

--- experiments.py
+++ experiments.py
@@ -16,31 +16,45 @@ def run_sim(run, strategy, scenario = 0):
         # print(sim.state.time)
         event = event_info[2]
         # print(event, sim.state.time)
-        if type(event) is StopTracking:
-            with open(f'Results/{strategy.__name__}/{strategy.__name__}_run_{run}.csv', 'a') as f:
-                writer = csv.writer(f)
-                # n_vehicles,percentage_delays,avg_delay,max_delay, total_delays, n_no_space
-                writer.writerow((sim.state.n_vehicles, sim.state.n_delays / sim.state.n_vehicles ,sim.state.delays_sum / sim.state.n_vehicles / 3600 ,sim.state.max_delay / 3600, sim.state.delays_sum, sim.state.n_no_space))
-
-            i = 0
-            for cable in sim.state.cables:
-                with open(f'Results/{strategy.__name__}/{strategy.__name__}_cables_{ct.SCENARIO}_run_{run}.csv', 'a') as f:
-                    writer = csv.writer(f)
-                    # max_load, % overload, % blackout
-                    writer.writerow((max(cable.loads, key = lambda l: l[1])[1], 100 * cable.overload / (float(sim.state.time) + 1), 100 * cable.blackout / (float(sim.state.time) + 1)))
-                i += 1
         event.event_handler(sim)
 
-    i = 0
-    for cable in sim.state.cables:
-        with open(f'Results/{strategy.__name__}/{strategy.__name__}_cable_{i}_{ct.SCENARIO}_run_{run}.csv', 'a') as f:
-            writer = csv.writer(f)
-            # time, load
-            writer.writerows(cable.loads)
-        i += 1
+        if type(event) is StopTracking:
+            # with open(f'Results/{strategy.__name__}/{strategy.__name__}_run_{run}.csv', 'a') as f:
+                # writer = csv.writer(f)
+                # # n_vehicles,percentage_delays,avg_delay,max_delay, total_delays, n_no_space
+                # writer.writerow((sim.state.n_vehicles, sim.state.n_delays / sim.state.n_vehicles ,sim.state.delays_sum / sim.state.n_vehicles / 3600 ,sim.state.max_delay / 3600, sim.state.delays_sum, sim.state.n_no_space))
+            
+            with open(f'Results/all_runs_stats.csv', 'a') as f:
+                writer = csv.writer(f)
+                # n_vehicles,percentage_delays,avg_delay,max_delay, total_delays, n_no_space
+                writer.writerow((run, strategy.__name__, ct.SCENARIO, sim.state.n_vehicles, sim.state.n_delays / sim.state.n_vehicles ,sim.state.delays_sum / sim.state.n_vehicles / 3600 ,sim.state.max_delay / 3600, sim.state.delays_sum, sim.state.n_no_space))
+
+            # i = 0
+            # for cable in sim.state.cables:
+            #     with open(f'Results/{strategy.__name__}/{strategy.__name__}_cables_{ct.SCENARIO}_run_{run}.csv', 'a') as f:
+            #         writer = csv.writer(f)
+            #         # max_load, % overload, % blackout
+            #         writer.writerow((max(cable.loads, key = lambda l: l[1])[1], 100 * cable.overload / (float(sim.state.time) + 1), 100 * cable.blackout / (float(sim.state.time) + 1)))
+            #     i += 1
+            
+            i = 0
+            for cable in sim.state.cables:
+                with open(f'Results/all_runs_cable_{i}.csv', 'a') as f:
+                    writer = csv.writer(f)
+                    # max_load, % overload, % blackout
+                    writer.writerow((run, strategy.__name__, ct.SCENARIO, max(cable.loads, key = lambda l: l[1])[1], 100 * cable.overload / (float(sim.state.time) + 1), 100 * cable.blackout / (float(sim.state.time) + 1)))
+                i += 1
+
+        i = 0
+        for cable in sim.state.cables:
+            with open(f'Results/{strategy.__name__}/{strategy.__name__}_cable_{i}_{ct.SCENARIO}_run_{run}.csv', 'a') as f:
+                writer = csv.writer(f)
+                # time, load
+                writer.writerows((run, ct.SCENARIO, sim.state.time, strategy.__name__, i, cable.loads))
+            i += 1
 
 if __name__ == '__main__':
-    for run in range(100):
+    for run in range(31, 100):
         for strategy in [s.BaseChargingStrategy, s.PriceDrivenChargingStrategy, s.FCFSChargingStrategy, s.ELFSChargingStrategy]:
             for scenario in range(5):
                 ct.SCENARIO = scenario
