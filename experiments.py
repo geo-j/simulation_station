5f36d2bb236d1bf28c4514b0a0b88656b0f5d7d9
geo-j <g.juglan@tutanota.com>
2021-05-28 11:53:27 +0200

    fix changing strategy

--- experiments.py
+++ experiments.py
@@ -7,37 +7,38 @@ import csv
 import constants as ct
 
 def run_sim(run, strategy, scenario = 0):
-    print(f'======= Run {run} for {strategy.__name__} in scenario {scenario} =======')
-    sim = simulation.Simulation(strategy)
-    init(sim)
+    print(f'======= Run {run} for {strategy.__name__} in scenario {ct.SCENARIO} =======')
+    sim = simulation.Simulation(strategy())
+    sim = init(sim)
     while not sim.events.empty():
+        # print(sim.strategy)
         event_info = sim.events.get()
         sim.state.time = event_info[0]
         # print(sim.state.time)
         event = event_info[2]
         # print(event, sim.state.time)
-        if type(event) is StopTracking:
-            with open(f'Results/{strategy.__name__}/{strategy.__name__}_run_{run}.csv', 'a') as f:
-                writer = csv.writer(f)
-                # n_vehicles,percentage_delays,avg_delay,max_delay, total_delays, n_no_space
-                writer.writerow((sim.state.n_vehicles, sim.state.n_delays / sim.state.n_vehicles ,sim.state.delays_sum / sim.state.n_vehicles / 3600 ,sim.state.max_delay / 3600, sim.state.delays_sum, sim.state.n_no_space))
+        # if type(event) is StopTracking:
+        #     with open(f'Results/{strategy.__name__}/{strategy.__name__}_run_{run}.csv', 'a') as f:
+        #         writer = csv.writer(f)
+        #         # n_vehicles,percentage_delays,avg_delay,max_delay, total_delays, n_no_space
+        #         writer.writerow((sim.state.n_vehicles, sim.state.n_delays / sim.state.n_vehicles ,sim.state.delays_sum / sim.state.n_vehicles / 3600 ,sim.state.max_delay / 3600, sim.state.delays_sum, sim.state.n_no_space))
 
-            i = 0
-            for cable in sim.state.cables:
-                with open(f'Results/{strategy.__name__}/{strategy.__name__}_cables_{ct.SCENARIO}_run_{run}.csv', 'a') as f:
-                    writer = csv.writer(f)
-                    # max_load, % overload, % blackout
-                    writer.writerow((max(cable.loads, key = lambda l: l[1])[1], 100 * cable.overload / (float(sim.state.time) + 1), 100 * cable.blackout / (float(sim.state.time) + 1)))
-                i += 1
+        #     i = 0
+        #     for cable in sim.state.cables:
+        #         with open(f'Results/{strategy.__name__}/{strategy.__name__}_cables_{ct.SCENARIO}_run_{run}.csv', 'a') as f:
+        #             writer = csv.writer(f)
+        #             # max_load, % overload, % blackout
+        #             writer.writerow((max(cable.loads, key = lambda l: l[1])[1], 100 * cable.overload / (float(sim.state.time) + 1), 100 * cable.blackout / (float(sim.state.time) + 1)))
+        #         i += 1
         event.event_handler(sim)
 
-    i = 0
-    for cable in sim.state.cables:
-        with open(f'Results/{strategy.__name__}/{strategy.__name__}_cable_{i}_{ct.SCENARIO}_run_{run}.csv', 'a') as f:
-            writer = csv.writer(f)
-            # time, load
-            writer.writerows(cable.loads)
-        i += 1
+    # i = 0
+    # for cable in sim.state.cables:
+    #     with open(f'Results/{strategy.__name__}/{strategy.__name__}_cable_{i}_{ct.SCENARIO}_run_{run}.csv', 'a') as f:
+    #         writer = csv.writer(f)
+    #         # time, load
+    #         writer.writerows(cable.loads)
+    #     i += 1
 
 if __name__ == '__main__':
     for run in range(100):
